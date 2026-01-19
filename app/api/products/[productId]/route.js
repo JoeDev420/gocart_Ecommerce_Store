f734c64c397f5fe63bcae9cbe0a1777bf3796b53
Anant Singh <anant.inbox@gmail.com>
2026-01-19 18:09:07 +0530

    stock updates 4

--- app/api/products/[productId]/route.js
+++ app/api/products/[productId]/route.js
@@ -3,12 +3,19 @@ import { NextResponse } from "next/server";
 
 export async function GET(_, { params }) {
   const product = await prisma.product.findUnique({
-    where: { id: params.id },
+    where: { id: params.productId },
     include: {
       rating: true,
       store: true,
     },
   });
+
+  if (!product || !product.store.isActive) {
+    return NextResponse.json(
+      { error: "Product not found" },
+      { status: 404 }
+    );
+  }
 
   return NextResponse.json({ product });
 }
